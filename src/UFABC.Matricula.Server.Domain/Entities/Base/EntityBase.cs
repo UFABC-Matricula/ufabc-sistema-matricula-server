fe1ec13bf2337556cf2dc1a6f1dee03c5aa182b9
Gabriela Tamura <gabitamu0103@outlook.com>
2022-10-31 22:14:15 -0300

    Initial setup with Aluno example

---
+++ src/UFABC.Matricula.Server.Domain/Entities/Base/EntityBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UFABC.Matricula.Server.Domain.Entities.Base
+{
+    public class EntityBase
+    {
+        public Guid Id { get; set; }
+    }
+}
