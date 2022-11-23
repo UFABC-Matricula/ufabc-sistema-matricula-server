b0bfddfdbb43b4a3616d1dca0512f4ad1dd8cf9a
Henrique Werneck <werneck.henrique@outlook.com>
2022-11-22 23:19:27 -0300

    Modelo de dados inicial dos alunos

--- src/UFABC.Matricula.Server.Repository/DataModels/AlunoDataModel.cs
+++ src/UFABC.Matricula.Server.Repository/DataModels/AlunoDataModel.cs
@@ -1,10 +1,56 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.ComponentModel.DataAnnotations;
 
-namespace UFABC.Matricula.Server.Repository.DataModels
+namespace Mvc_MongoDB.Models
 {
-    class AlunoDataModel
+
+    private enum CampusEnum
     {
+        Santo André,
+        São Bernardo do Campo,
+        MAX_CAMPI
+    }
+
+    private enum TurnoEnum
+    {
+        Matutino,
+        Noturno,
+        MAX_TURNOS
+    }
+
+    struct CursoDeGraduacao
+    {
+        string Curso;
+        CampusEnum Campus;
+        TurnoEnum Turno;
+        double CP;
+    }
+
+    public class AlunoDataModel
+    {
+        [BsonRepresentation(BsonType.ObjectId)]
+        private string Id {get; set;}
+
+        [Required]
+        [Display(Name = "RA")]
+        private string RA {get; set;}
+
+        [Required]
+        [Display(Name = "Nome")]
+        private string Nome {get; set;}
+
+        [Required]
+        [Display(Name = "Login")]
+        private string Login {get; set;}
+
+        [Required]
+        [Display(Name = "Senha")]
+        private string Senha {get; set;}
+
+        [Required]
+        [Display(Name = "Cursos de Graduação")]
+        private CursoDeGraduacao CursosDeGraduacao {get; set;}
     }
 }
